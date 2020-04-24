c201098032d6b63fd7b59bec64317bd2ebb43cb6
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-04-24 19:17:41 +0200

    Port over code from puppet-foreman
    These files were part of puppet-foreman but now become a standalone
    module. This makes it easier to test.
    
    Various fixes were made to get tests to pass properly in the new layout
    and with the additional operating systems. No actual code was changed.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Puppet module for managing Foreman integration in Puppetserver
+
+The Foreman integration consists of an ENC and a report processor. This has a
+configuration file. All of this can be managed by this module.
+
+Historically this integration was part of [theforeman-foreman
+module](https://github.com/theforeman/puppet-foreman).
+
+## Compatibility
+
+Foreman API v2: 1.3 - 2.x
+Puppetserver: 1.x - 6.x
+
+These scripts have a long history and have basically been unchanged since
+Puppet 2.6, even before Puppetserver existed. Since they haven't dropped code,
+they probably still work.
---
+++ manifests/params.pp
@@ -0,0 +1,53 @@
+# Defaults for the main class that are fact dependent
+# @api private
+class puppetserver_foreman::params {
+  $lower_fqdn = downcase($facts['networking']['fqdn'])
+  $foreman_url = "https://${lower_fqdn}"
+
+  if fact('aio_agent_version') =~ String[1] {
+    $puppet_basedir = '/opt/puppetlabs/puppet/lib/ruby/vendor_ruby/puppet'
+    $puppet_etcdir = '/etc/puppetlabs/puppet'
+    $puppet_home = '/opt/puppetlabs/server/data/puppetserver'
+    $puppet_ssldir = '/etc/puppetlabs/puppet/ssl'
+  } else {
+    case $facts['os']['family'] {
+      'RedHat': {
+        $puppet_basedir  = '/usr/share/ruby/vendor_ruby/puppet'
+        $puppet_etcdir = '/etc/puppet'
+        $puppet_home = '/var/lib/puppet'
+      }
+      'Debian': {
+        $puppet_basedir  = '/usr/lib/ruby/vendor_ruby/puppet'
+        $puppet_etcdir = '/etc/puppet'
+        $puppet_home = '/var/lib/puppet'
+      }
+      'Archlinux': {
+        # lint:ignore:legacy_facts
+        $puppet_basedir = regsubst($facts['rubyversion'], '^(\d+\.\d+).*$', '/usr/lib/ruby/vendor_ruby/\1/puppet')
+        # lint:endignore
+        $puppet_etcdir = '/etc/puppetlabs/puppet'
+        $puppet_home = '/var/lib/puppet'
+      }
+      /^(FreeBSD|DragonFly)$/: {
+        # lint:ignore:legacy_facts
+        $puppet_basedir = regsubst($facts['rubyversion'], '^(\d+\.\d+).*$', '/usr/local/lib/ruby/site_ruby/\1/puppet')
+        # lint:endignore
+        $puppet_etcdir = '/usr/local/etc/puppet'
+        $puppet_home = '/var/puppet'
+      }
+      default: {
+        $puppet_basedir = undef
+        $puppet_etcdir = undef
+        $puppet_home = undef
+      }
+    }
+
+    $puppet_ssldir = "${puppet_home}/ssl"
+  }
+
+  # If CA is specified, remote Foreman host will be verified in reports/ENC scripts
+  $client_ssl_ca   = "${puppet_ssldir}/certs/ca.pem"
+  # Used to authenticate to Foreman, required if require_ssl_puppetmasters is enabled
+  $client_ssl_cert = "${puppet_ssldir}/certs/${lower_fqdn}.pem"
+  $client_ssl_key  = "${puppet_ssldir}/private_keys/${lower_fqdn}.pem"
+}
